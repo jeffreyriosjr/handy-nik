5f849e043bc7192479edc8c9a6736c9f8b8fa4ee
jeffrey rios jr <jeffriosjr@gmail.com>
2022-05-26 09:18:59 -0700

    updated contact page sidebar

--- page-contact-us.php
+++ page-contact-us.php
@@ -17,7 +17,7 @@ $contact_us =get_field('contact_us');
 		<div class="contact-yan container-fluid row d-flex" style="background-image: url('<?php echo $background_image; ?> '); background-size: cover; background-repeat: no-repeat;">
 		<div class="col-sm-6 m-5">
 		<h1 class="contact-heading" data-aos="slide-left" data-aos-easing="ease-in" data-aos-duration="1000"><?php echo $contact_heading; ?>
-			Hello!</h1>
+			</h1>
 		</div>
 			<div class="col-sm-6 second-col pt-5">
 					<?php echo $contact_form_section; ?>
@@ -28,5 +28,5 @@ $contact_us =get_field('contact_us');
         
     </main>
 <?php
-get_sidebar();
+
 get_footer();
